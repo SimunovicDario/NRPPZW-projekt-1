539711b3b5ed4e13d0ce52045b9537aa2bbfce40
SimunovicDario <dario.simunovic@fer.hr>
2021-11-03 14:44:55 +0100

    Cleanup

--- index.js
+++ index.js
@@ -33,45 +33,26 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, './home.html'));
 });
 
-app.get('/leaflet.css', function (req, res) {
-	res.sendFile(path.join(__dirname, './leaflet/leaflet.css'));
-});
-
-app.get('/leaflet.js', function (req, res) {
-	res.sendFile(path.join(__dirname, './leaflet/leaflet.js'));
-});
-
-app.get('/images/:image', function (req, res) {
-	const image = req.params.image
-	res.sendFile(path.join(__dirname, './leaflet/images/'+image));
+app.get('/leaflet/*', function (req, res) {
+	res.sendFile(path.join(__dirname, req.url));
 });
 
 app.get('/favicon.ico', function (req, res) {
 	res.sendFile(path.join(__dirname, './favicon.ico'));
 });
 
-
-let last5Users = []
+let last5 = []
 app.post('/location', (req, res) => {
 	if(!req.oidc.user) return res.status(200).json(null)
-	const mail = req?.oidc?.user.email
-	if(mail) {
-		const index = last5Users.findIndex(user=> user.mail === mail)
-		if(index !== -1) last5Users.splice(index, 1)
-		const jitter = Math.random()*0.0005*(Math.random > 0.5 ? 1 : -1) // dodan jitter kako bi se vidjeli preklapajući markeri s istog kompjutera
-		const position = {longitude: jitter+req.body.longitude, latitude: jitter+req.body.latitude}
-		last5Users.push({date: new Date(), position, mail, name: req.oidc.user.name})
+	const mail = req.oidc.user.email ?? null
+	if(mail) { // ako mail postoji korisnik je prijavljen
+		const index = last5.findIndex(user=> user.mail === mail)
+		if(index !== -1) last5.splice(index, 1)
+		const jitter1 = Math.random()*0.0005*(Math.random > 0.5 ? 1 : -1) // dodan jitter kako bi se vidjeli preklapajući markeri s istog kompjutera
+		const jitter2 = Math.random()*0.0005*(Math.random > 0.5 ? 1 : -1)
+		const position = {longitude: jitter1+req.body.longitude, latitude: jitter2+req.body.latitude}
+		last5.push({date: new Date(), position, mail, name: req.oidc.user.name})
 	}
-	while(last5Users.length > 5) last5Users.shift()
-	res.status(200).json({me:req.oidc.user, list: last5Users})
+	while(last5.length > 5) last5.shift()
+	res.status(200).json({me:req.oidc.user, list: last5})
 });
-
-app.set('trust proxy', true);
-app.use((err, req, res, next) => {
-	if (res.statusCode.toString().match(/^2.{2}$/)) {
-		res.status(500);
-	}
-	res.send(err.message);
-});
-
-console.log("Server started at: 3000")
